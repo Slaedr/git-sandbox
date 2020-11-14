40a5ba35e442f95ab15c73f96d56aebdf0c94cfb
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 12:21:12 +0100

    Update format.sh

--- .github/format.sh
+++ .github/format.sh
@@ -7,12 +7,12 @@ UNTRIMMED_COMMITTER_TOKEN=${!USER_TOKEN:-$GITHUB_TOKEN}
 COMMITTER_TOKEN="$(echo -e "${UNTRIMMED_COMMITTER_TOKEN}" | tr -d '[:space:]')"
 
 git remote set-url origin https://x-access-token:$COMMITTER_TOKEN@github.com/$GITHUB_REPOSITORY.git
-git config --global user.email "$USER_EMAIL"
-git config --global user.name "$USER_NAME"
+git config --global user.email "bot@upsj.de"
+git config --global user.name "bot"
 
 set -o xtrace
 
-for f in *.cpp; do clang-format -i $f; done
+for f in *.cpp; do clang-format-8 -i $f; done
 
 git commit *.cpp -m 'Format code'
 
