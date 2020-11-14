7c287167197bb8f60d1054b6998f824a4339cd98
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 17:46:11 +0100

    Update label.sh

--- .github/label.sh
+++ .github/label.sh
@@ -4,7 +4,19 @@ source $(dirname "${BASH_SOURCE[0]}")/bot-base.sh
 
 echo -n "Collecting information on triggering PR"
 PR_URL=$(jq -er .pull_request.url "$GITHUB_EVENT_PATH")
+echo -n .
+ISSUE_URL=$(jq -er .pull_request.issue_url "$GITHUB_EVENT_PATH")
 echo .
 
+echo "Retrieving PR file list"
 PR_FILES=$(api_get "$PR_URL/files?&per_page=1000" | jq -er '.[] | .filename')
-echo $PR_FILES
+
+echo "Retrieving PR label list"
+OLD_LABELS=$(api_get "$ISSUE_URL" | jq -er '[.labels | .[] | .name]')
+
+NEW_LABELS="[]"
+if echo "$PR_FILES" | grep .cpp; then
+  NEW_LABELS="$NEW_LABELS + [\"bug\"]"
+fi
+echo $OLD_LABELS, $NEW_LABELS
+NEW_LABELS=$(jq -rn "$OLD_LABELS + $NEW_LABELS | unique)
