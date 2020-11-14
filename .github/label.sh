539004c9ddd16e946760fa949742df23a5839fdb
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 17:25:19 +0100

    Create label.sh

---
+++ .github/label.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source $(dirname "${BASH_SOURCE[0]}")/bot-base.sh
+
+echo -n "Collecting information on triggering PR"
+PR_URL=$(jq -er .pull_request.url "$GITHUB_EVENT_PATH")
+echo .
+
+PR_FILES=$(api_get "PR_URL/files?&per_page=1000" | jq -er '.[] | .filename')
+echo $PR_FILES
