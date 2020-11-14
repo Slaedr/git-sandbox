8b69dc2bd6b6c34a418d89c8bd5232d08bff94dd
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 19:26:57 +0100

    Update bot-pr-comment-base.sh

--- .github/bot-pr-comment-base.sh
+++ .github/bot-pr-comment-base.sh
@@ -25,7 +25,7 @@ echo -n "Collecting information on pull request"
 PR_JSON=$(api_get $PR_URL)
 echo -n .
 
-PR_MERGED=$(echo "$PR_JSON" | jq -r .merged')
+PR_MERGED=$(echo "$PR_JSON" | jq -r .merged)
 echo -n .
 BASE_REPO=$(echo "$PR_JSON" | jq -er .base.repo.full_name)
 echo -n .
