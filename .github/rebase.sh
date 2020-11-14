95e19bc3df73622f773ed06208295f5a0296e5c5
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 14:55:41 +0100

    Update rebase.sh

--- .github/rebase.sh
+++ .github/rebase.sh
@@ -2,15 +2,11 @@
 
 source $(dirname "${BASH_SOURCE[0]}")/bot-base.sh
 
-set -e
-
 git remote set-url origin "$BASE_URL"
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
 
 git remote add fork "$HEAD_URL"
-
-set -o xtrace
 
 # make sure branches are up-to-date
 git fetch origin $BASE_BRANCH
@@ -18,7 +14,9 @@ git fetch fork $HEAD_BRANCH
 
 # do the rebase
 git checkout -b $HEAD_BRANCH fork/$HEAD_BRANCH
-git rebase origin/$BASE_BRANCH
+git rebase origin/$BASE_BRANCH || bot_error "Rebasing failed"
 
 # push back
 git push --force-with-lease fork $HEAD_BRANCH
+
+bot_comment "Rebasing succeeded"
