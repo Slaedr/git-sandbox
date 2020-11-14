8538d5a22a5a2dc1127c1597befdcd3bbdf82a4b
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 13:06:04 +0100

    Update bot-base.sh

--- .github/bot-base.sh
+++ .github/bot-base.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
+
 API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 ISSUE_URL=$(jq -r ".issue.url" "$GITHUB_EVENT_PATH")
+
 if [[ "$ISSUE_URL" == "null" ]]; then
   echo "Could not determine issue URL"
   exit 1
@@ -18,10 +24,21 @@ api_post() {
   curl -X POST -s -H "${AUTH_HEADER}" -H "${API_HEADER}" "$1" -d "$2"
 }
 
+bot_comment() {
+  api_post $ISSUE_URL/comments "{\"body\":\"$1\"}" | jq -r .url
+}
+
 bot_error() {
   echo "$1"
-  api_post $ISSUE_URL/comments "{\"body\":\"Error: $1\"}"
+  bot_comment "Error: $1" > /dev/null
   exit 1
 }
+
+PR_URL=$(jq -r ".pull_request.url" "$GITHUB_EVENT_PATH")
+PR_NUMBER=$(jq -r ".pull_request.number" "$GITHUB_EVENT_PATH")
+
+if [[ "PR_URL" == "null" ]]; then
+  error "I can only operate on PRs!"
+fi
 
 set -o xtrace
