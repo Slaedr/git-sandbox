cb8c22543d9fdd71579764c213443144355ccbab
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 12:50:35 +0100

    Update format.sh

--- .github/format.sh
+++ .github/format.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-set -e
+source $(dirname "${BASH_SOURCE[0]}")/bot-base.sh
 
-USER_TOKEN=${USER_LOGIN//-/_}_TOKEN
-UNTRIMMED_COMMITTER_TOKEN=${!USER_TOKEN:-$GITHUB_TOKEN}
-COMMITTER_TOKEN="$(echo -e "${UNTRIMMED_COMMITTER_TOKEN}" | tr -d '[:space:]')"
-
-git remote set-url origin https://x-access-token:$COMMITTER_TOKEN@github.com/$GITHUB_REPOSITORY.git
-git config --global user.email "bot@upsj.de"
-git config --global user.name "bot"
-
-set -o xtrace
-
-for f in *.cpp; do clang-format-8 -i $f; done
-
-git commit *.cpp -m 'Format code'
-
-# push back
-git push
+error "Testfehler!"
