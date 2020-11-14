9b0f6e810b5209715af8a4426e4659a69d0bd516
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 14:51:31 +0100

    Update rebase.sh

--- .github/rebase.sh
+++ .github/rebase.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-git remote set-url origin "$BASE_URL"
+git remote add base "$BASE_URL"
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
 
@@ -11,12 +11,12 @@ git remote add fork "$HEAD_URL"
 set -o xtrace
 
 # make sure branches are up-to-date
-git fetch origin $BASE_BRANCH
+git fetch base $BASE_BRANCH
 git fetch fork $HEAD_BRANCH
 
 # do the rebase
 git checkout -b $HEAD_BRANCH fork/$HEAD_BRANCH
-git rebase origin/$BASE_BRANCH
+git rebase base/$BASE_BRANCH
 
 # push back
 git push --force-with-lease fork $HEAD_BRANCH
