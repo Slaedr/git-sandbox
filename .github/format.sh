d1cf8cebfcde1d783af35bb2689b59549bcdbf29
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 14:24:51 +0100

    Update format.sh

--- .github/format.sh
+++ .github/format.sh
@@ -6,6 +6,8 @@ if [[ "$BASE_REPO" != "$HEAD_REPO" ]]; then
   error "I can't operate on forks!"
 fi
 
+git config user.email "bot@upsj.de"
+git config user.name "Bot"
 git remote add tmp-origin "$HEAD_URL"
 git fetch tmp-origin
 git checkout tmp-origin/$HEAD_BRANCH
@@ -17,7 +19,7 @@ for f in *.cpp; do
     cat tmp > $f
   fi
 done
-git commit *.cpp -m "Format files\n\nCo-authored-by: USER_COMBINED"
+git commit *.cpp -m "Format files\n\nCo-authored-by: $USER_COMBINED"
 git push
 if [[ "$LIST_FILES" != "yes" ]]; then
   bot_comment "Formatted the following files:\n```$LIST_FILES\n```"
