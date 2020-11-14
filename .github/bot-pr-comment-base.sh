405c5f73f0b227242613a80eb841541dd0580f01
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 19:20:25 +0100

    Update bot-pr-comment-base.sh

--- .github/bot-pr-comment-base.sh
+++ .github/bot-pr-comment-base.sh
@@ -26,7 +26,7 @@ PR_JSON=$(api_get $PR_URL)
 echo -n .
 echo "$PR_JSON"
 
-PR_MERGED=$(echo "$PR_JSON" | jq -er .merged)
+PR_MERGED=$(echo "$PR_JSON" | jq -er 'if (.merged != null) then .merged else false`)
 echo -n .
 BASE_REPO=$(echo "$PR_JSON" | jq -er .base.repo.full_name)
 echo -n .
