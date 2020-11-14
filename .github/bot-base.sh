79113ab5d42f6281823db5d87f7b3b2807f89bb3
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 13:35:29 +0100

    Update bot-base.sh

--- .github/bot-base.sh
+++ .github/bot-base.sh
@@ -2,19 +2,11 @@
 
 set -e
 
-if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "Set the GITHUB_TOKEN env variable."
-	exit 1
-fi
-
 API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
-ISSUE_URL=$(jq -r ".issue.url" "$GITHUB_EVENT_PATH")
-
-if [[ "$ISSUE_URL" == "null" ]]; then
-  echo "Could not determine issue URL"
-  exit 1
-fi
+ISSUE_URL=$(jq -er ".issue.url" "$GITHUB_EVENT_PATH")
+USER_LOGIN=$(jq -er ".comment.user.login" "$GITHUB_EVENT_PATH")
+USER_URL=$(jq -er ".comment.user.url" "$GITHUB_EVENT_PATH")
 
 api_get() {
   curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" "$1"
@@ -25,20 +17,33 @@ api_post() {
 }
 
 bot_comment() {
-  api_post $ISSUE_URL/comments "{\"body\":\"$1\"}" | jq -r .url
+  api_post $ISSUE_URL/comments "{\"body\":\"$1\"}" > /dev/null
 }
 
 bot_error() {
   echo "$1"
-  bot_comment "Error: $1" > /dev/null
+  bot_comment "Error: $1"
   exit 1
 }
 
-PR_URL=$(jq -r ".pull_request.url" "$GITHUB_EVENT_PATH")
-PR_NUMBER=$(jq -r ".pull_request.number" "$GITHUB_EVENT_PATH")
+PR_URL=$(jq -er ".pull_request.url" "$GITHUB_EVENT_PATH")
+PR_NUMBER=$(jq -er ".pull_request.number" "$GITHUB_EVENT_PATH")
 
 if [[ "PR_URL" == "null" ]]; then
   error "I can only operate on PRs!"
 fi
 
-set -o xtrace
+# collect info on the PR we are checking
+PR_JSON=$(api_get $PR_URL)
+
+BASE_REPO=$(echo "$PR_JSON" | jq -er .base.repo.full_name)
+BASE_BRANCH=$(echo "$PR_JSON" | jq -er .base.ref)
+HEAD_REPO=$(echo "$PR_JSON" | jq -er .head.repo.full_name)
+HEAD_BRANCH=$(echo "$PR_JSON" | jq -er .head.ref)
+
+# collect info on the user that invoked the bot
+USER_JSON=$(api_get $USER_URL)
+
+USER_NAME=$(echo "$USER_JSON" | jq -er ".name" || echo "Unknown")
+USER_EMAIL=$(echo "$USER_JSON" | jq -er ".email" || echo "unknown@nowhere")
+USER_COMBINED="$USER_NAME <$USER_EMAIL>"
