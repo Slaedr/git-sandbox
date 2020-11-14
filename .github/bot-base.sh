95bb96b1d2f67bce304e7c2ae2b19dddf71f94f8
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 14:05:15 +0100

    Update bot-base.sh

--- .github/bot-base.sh
+++ .github/bot-base.sh
@@ -41,6 +41,6 @@ HEAD_BRANCH=$(echo "$PR_JSON" | jq -er .head.ref)
 # collect info on the user that invoked the bot
 USER_JSON=$(api_get $USER_URL)
 
-USER_NAME=$(echo "$USER_JSON" | jq -er ".name")
-USER_EMAIL=$(echo "$USER_JSON" | jq -er ".email")
+USER_NAME=$(echo "$USER_JSON" | jq -er ".name" || echo "$USER_LOGIN")
+USER_EMAIL=$(echo "$USER_JSON" | jq -er ".email" || echo "$USER_LOGIN@users.noreply.github.com")
 USER_COMBINED="$USER_NAME <$USER_EMAIL>"
