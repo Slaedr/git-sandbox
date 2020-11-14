6a08453a3f73a0ad0f88be91e6f35c6f10108a3f
Tobias Ribizel <ribizel@kit.edu>
2020-11-14 17:31:47 +0100

    Update label.sh

--- .github/label.sh
+++ .github/label.sh
@@ -6,5 +6,5 @@ echo -n "Collecting information on triggering PR"
 PR_URL=$(jq -er .pull_request.url "$GITHUB_EVENT_PATH")
 echo .
 
-PR_FILES=$(api_get "PR_URL/files?&per_page=1000" | jq -er '.[] | .filename')
+PR_FILES=$(api_get "$PR_URL/files?&per_page=1000" | jq -er '.[] | .filename')
 echo $PR_FILES
